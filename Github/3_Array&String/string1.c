49291846a5e9d04fdf41d1c3cb84ca8f7407af5d
KamalBaria <97674214+KamalBaria@users.noreply.github.com>
2022-01-25 15:12:56 +0530

    Delete Github directory

--- Github/3_Array&String/string1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void main()
-{
-    char a[10] = {'I','A','R'};
-    printf("%s",a);
-
-    return 0;
-}
--- Github/5_Pointers_Structure&Union/Null.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int *x = NULL;
-    printf("Value of x: %x",x);
-}
